0e01534f7b25b553fe337f892d7eacf62110d8da
khai96_ <hvksmr1996@gmail.com>
2017-03-15 08:18:43 +0700

    Check mtime before doing compilation

--- script/compile.js
+++ script/compile.js
@@ -23,9 +23,12 @@ function compile (source, target, level) {
     const sourcecode = readFileSync(source)
     const {dir, name} = parse(target)
     rgxmap.some(([regex, suffix, compile]) => {
-      if (regex.test(source)) {
-        const target = join(dir, name + suffix)
-        const locals = {projdir, src, out, source, target, dir, name, sourcecode, require, getlib, jreq}
+      if (!regex.test(source)) return false
+      const target = join(dir, name + suffix)
+      const sourcemtime = stats.mtime
+      const targetmtime = jtry(() => statSync(target).mtime, () => Infinity)
+      if (sourcemtime > targetmtime) {
+        const locals = {projdir, src, out, source, target, dir, name, sourcecode, require, getlib, jreq, sourcemtime, targetmtime}
         const output = compile(sourcecode, locals)
         writeFileSync(target, output)
         return true
