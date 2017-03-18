f7b049faedaa49a85e50c2a2ab2b815c283f0ad7
khai96_ <hvksmr1996@gmail.com>
2017-03-18 12:11:30 +0700

    Complete get-deps-tree

--- script/get-deps-tree.js
+++ script/get-deps-tree.js
@@ -1,11 +1,27 @@
 'use strict'
 
 const {dirname, join} = require('path')
-const {readFileSync} = require('fs')
+const {readFileSync, statSync, readdirSync} = require('fs')
 const merge = require('deepmerge')
+const jsDepTree = require('dependency-tree').toList
 const jtry = require('just-try')
 const {projdir, dep, tryReadJSON} = require('../lib/common-vars.js')
 const depYAML = require('./dep-map-yml.js')
 const depJSON = tryReadJSON(join(dep, 'dependencies.json'))
+const depNODE = {}
 
-module.exports = merge(depYAML, depJSON)
+function genNodeDeps (name) {
+  const stats = statSync(name)
+  if (stats.isFile()) {
+    if (/\.js$/.test(name)) {
+      const filter = string => !/node_modules/.test(string)
+      depNODE[name] = jsDepTree({filename: name, directory: projdir, filter})
+    }
+  } else if (stats.isDirectory()) {
+    readdirSync(name).map(item => join(name, item)).forEach(genNodeDeps)
+  } else {
+    throw new Error(`Unreconizable file system entry: ${name}`)
+  }
+}
+
+module.exports = merge.all([depYAML, depJSON, depNODE])
