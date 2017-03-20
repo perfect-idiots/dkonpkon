791b49fbb52a772c0266ce25d1638d54038ab0ae
khai96_ <hvksmr1996@gmail.com>
2017-03-20 23:47:45 +0700

    Basic of search-button

---
+++ src/script/search.js
@@ -0,0 +1,27 @@
+(function (window) {
+  'use strict'
+  const {document} = window
+  const {body} = document
+  // const input = document.getElementById('search-input')
+
+  const toggleSearchBox = () =>
+    isSearchBoxShown() ? hideSearchBox() : showSearchBox()
+
+  const isSearchBoxShown = () =>
+    body.classList.contains('show-search-box')
+
+  const showSearchBox = () =>
+    body.classList.add('show-search-box')
+
+  const hideSearchBox = () =>
+    body.classList.remove('show-search-box')
+
+  document
+    .getElementById('search-button')
+    .addEventListener('click', event => {
+      event.stopPropagation()
+      toggleSearchBox()
+    }, false)
+
+  body.addEventListener('click', hideSearchBox, false)
+}).call(window, window)
