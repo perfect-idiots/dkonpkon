7cc378a2ea0c3aa7e5decb38b14f19042a5a15e3
khai96_ <hvksmr1996@gmail.com>
2017-03-23 23:37:01 +0700

    Complete flash.js

--- src/script/flash.js
+++ src/script/flash.js
@@ -5,11 +5,30 @@
   const allArticleContainer = document.querySelectorAll('[target-game-item]')
 
   for (const articleContainer of allArticleContainer) {
+    const targetGameItem = articleContainer.getAttribute('target-game-item')
     const button = document.createElement('button')
     articleContainer.appendChild(button)
     button.textContent = 'chơi ngay'
-    button.addEventListener('click', onClickPlayNow, false)
+    button.addEventListener('click', () => openNewGame(targetGameItem), false)
+    button.classList.add('play')
   }
 
-  function onClickPlayNow () {}
+  function openNewGame (targetGameItem) {
+    closeCurrentGame()
+
+    const embed = document.createElement('embed')
+    mainEmbedContainer.appendChild(embed)
+    embed.type = 'application/x-shockwave-flash'
+    embed.src = '../media/swf/' + targetGameItem
+
+    const close = document.createElement('button')
+    mainEmbedContainer.appendChild(close)
+    close.textContent = 'đóng'
+    close.addEventListener('click', closeCurrentGame, false)
+    close.classList.add('close')
+  }
+
+  function closeCurrentGame () {
+    mainEmbedContainer.textContent = ''
+  }
 }).call(window, window)
