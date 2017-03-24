26ac4126d7414f9f99926f216af2d070eb947d4d
khai96_ <hvksmr1996@gmail.com>
2017-03-24 20:53:42 +0700

    Create lib.js

---
+++ src/script/lib.js
@@ -0,0 +1,21 @@
+(function (window) {
+  const {defineProperty, assign} = Object
+  const defineConst = (object, name, value) => defineProperty(object, name, {value, writable})
+
+  const out = {
+    onResizeWindow () {
+      const mediaElement = document.querySelector('.media')
+      if (!mediaElement) return
+      mediaElement.width = window.innerWidth
+      mediaElement.height = window.innerHeight
+    }
+
+    get fromLibJs () {
+      return assign({}, out)
+    }
+  }
+
+  for (const name in out) {
+    defineConst(window, name, out[name])
+  }
+})(window, window)
