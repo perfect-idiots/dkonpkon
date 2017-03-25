b799e322d337cdd0bcf6333497821d206d73fc48
khai96_ <hvksmr1996@gmail.com>
2017-03-25 16:25:25 +0700

    Migrate flash.js main content to lib.js as mediaCommonAction()

--- src/script/flash.js
+++ src/script/flash.js
@@ -1,35 +1,5 @@
 (function (window) {
   'use strict'
-  const {document, onResizeWindow} = window
-  const mainEmbedContainer = document.getElementById('main-embed-container')
-  const allArticleContainer = document.querySelectorAll('[target-game-item]')
-
-  for (const articleContainer of allArticleContainer) {
-    const targetGameItem = articleContainer.getAttribute('target-game-item')
-    const button = document.createElement('button')
-    articleContainer.appendChild(button)
-    button.addEventListener('click', () => openNewGame(targetGameItem), false)
-    button.classList.add('play')
-  }
-
-  function openNewGame (targetGameItem) {
-    closeCurrentGame()
-
-    const embed = document.createElement('embed')
-    mainEmbedContainer.appendChild(embed)
-    embed.classList.add('media')
-    onResizeWindow()
-    embed.type = 'application/x-shockwave-flash'
-    embed.src = '../media/swf/' + targetGameItem
-
-    const close = document.createElement('button')
-    mainEmbedContainer.appendChild(close)
-    close.textContent = 'đóng'
-    close.addEventListener('click', closeCurrentGame, false)
-    close.classList.add('close')
-  }
-
-  function closeCurrentGame () {
-    mainEmbedContainer.textContent = ''
-  }
+  const {mediaCommonAction} = window
+  mediaCommonAction('embed', 'application/x-shockwave-flash', '../media/swf')
 }).call(window, window)
--- src/script/lib.js
+++ src/script/lib.js
@@ -3,15 +3,53 @@
   const defineConst = (object, name, value) => defineProperty(object, name, {value, writable: false})
 
   const out = {
-    onResizeWindow () {
-      const mediaElement = document.querySelector('.media')
-      if (!mediaElement) return
-      mediaElement.width = window.innerWidth
-      mediaElement.height = window.innerHeight
-    }
+    onResizeWindow,
+    mediaCommonAction
   }
 
   for (const name in out) {
     defineConst(window, name, out[name])
+  }
+
+  function onResizeWindow () {
+    const mediaElement = document.querySelector('.media')
+    if (!mediaElement) return
+    mediaElement.width = window.innerWidth
+    mediaElement.height = window.innerHeight
+  }
+
+  function mediaCommonAction (tagName, type, directory) {
+    const {document} = window
+    const mainEmbedContainer = document.querySelector('.media-container')
+    const allArticleContainer = document.querySelectorAll('[target-game-item]')
+
+    for (const articleContainer of allArticleContainer) {
+      const targetGameItem = articleContainer.getAttribute('target-game-item')
+      const button = document.createElement('button')
+      articleContainer.appendChild(button)
+      button.addEventListener('click', () => openNewGame(targetGameItem), false)
+      button.classList.add('play')
+    }
+
+    function openNewGame (targetGameItem) {
+      closeCurrentGame()
+
+      const player = document.createElement(tagName)
+      mainEmbedContainer.appendChild(player)
+      player.classList.add('media')
+      onResizeWindow()
+      player.type = type
+      player.src = directory + '/' + targetGameItem
+
+      const close = document.createElement('button')
+      mainEmbedContainer.appendChild(close)
+      close.textContent = 'đóng'
+      close.addEventListener('click', closeCurrentGame, false)
+      close.classList.add('close')
+    }
+
+    function closeCurrentGame () {
+      mainEmbedContainer.textContent = ''
+    }
   }
 })(window, window)
