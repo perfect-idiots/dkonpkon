55df29b47e1fb63029fed86121172a4762a981f5
Cuong <cuong8321@gmail.com>
2017-03-26 15:17:59 +0700

    Hoàn thành search.js

--- src/script/search.js
+++ src/script/search.js
@@ -2,8 +2,34 @@
   'use strict'
   const {document} = window
   const {body} = document
-  const input = document.getElementById('search-input')
+  const searchTextBox = document.getElementById('search-input')
   const menuVisibilityCheckbox = document.getElementById('menu-visibility-checkbox')
+  const list = document.querySelectorAll('#main-list .article-container')
+  const filterFieldSelect = document.getElementById('filter-field-select')
+  const caseSensitiveCheckbox = document.getElementById('case-sensitive-checkbox')
+
+  const diacritic = {
+    a: 'áàảãạăắằẳẵặâấầẩẫậ',
+    d: 'đ',
+    e: 'éèẻẽẹêếềểễệ',
+    i: 'íìỉĩị',
+    o: 'óòỏõọôốồổỗộơờởỡợ',
+    u: 'úùủũụưứừửữự',
+    y: 'ýỳỷỹỵ'
+  }
+  for (const nonDiaChar in diacritic) {
+    diacritic[nonDiaChar.toUpperCase()] = diacritic[nonDiaChar].toUpperCase()
+  }
+
+  const reverseDiacritic = {}
+  for (const nonDiaChar in diacritic) {
+    for (const diaChar of diacritic[nonDiaChar]) {
+      reverseDiacritic[diaChar] = nonDiaChar
+    }
+  }
+
+  const getNonDiaStr = diaString =>
+    Array.from(diaString).map(diaChar => reverseDiacritic[diaChar] || diaChar).join('')
 
   const toggleSearchBox = () =>
     isSearchBoxShown() ? hideSearchBox() : showSearchBox()
@@ -22,17 +48,17 @@
     .addEventListener('click', event => {
       event.stopPropagation()
       menuVisibilityCheckbox.checked = false
-      input.value ? showSearchResult() : toggleSearchBox()
+      searchTextBox.value ? showSearchResult() : toggleSearchBox()
     }, false)
 
-  document.addEventListener('click', () => input.value || hideSearchBox(), false)
-  input.addEventListener('keydown', onkeydown, false)
-  input.addEventListener('click', event => event.stopPropagation(), false)
+  document.addEventListener('click', () => searchTextBox.value || hideSearchBox(), false)
+  searchTextBox.addEventListener('keydown', onkeydown, false)
+  searchTextBox.addEventListener('click', event => event.stopPropagation(), false)
 
   function onkeydown ({keyCode}) {
     if (keyCode === 27) {
-      if (input.value) {
-        input.value = ''
+      if (searchTextBox.value) {
+        searchTextBox.value = ''
       } else {
         hideSearchBox()
       }
@@ -43,7 +69,31 @@
     }
   }
 
-  function showSuggestionList () {}
+  function showSuggestionList () {
+    showSearchResult()
+  }
 
-  function showSearchResult () {}
+  function showSearchResult () {
+    const getText = caseSensitiveCheckbox.checked
+      ? string => string
+      : string => string.toUpperCase()
+    const text = getText(searchTextBox.value)
+    const filterField = filterFieldSelect.value
+    const getContentElement = filterField === 'all'
+      ? articleContainer => articleContainer
+      : articleContainer => articleContainer.querySelector('.' + filterField)
+
+    for (const articleContainer of list) {
+      const content = getText(getContentElement(articleContainer).textContent)
+      articleContainer.hidden = check(content, text)
+    }
+
+    function check (content, text) {
+      return notSubStr(content, text) && notSubStr(getNonDiaStr(content), text)
+    }
+
+    function notSubStr (container, substring) {
+      return container.indexOf(substring) === -1
+    }
+  }
 }).call(window, window)
