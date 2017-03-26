9d1aebfe083a5fc9dfef8aa65d82d30bb07185b8
khai96_ <hvksmr1996@gmail.com>
2017-03-26 23:47:02 +0700

    Move things to common-search.js

---
+++ src/script/common-search.js
@@ -0,0 +1,45 @@
+(function (window) {
+  'use strict'
+  const {document} = window
+  const {body} = document
+  const searchTextBox = document.getElementById('search-input')
+  const menuVisibilityCheckbox = document.getElementById('menu-visibility-checkbox')
+
+  const toggleSearchBox = () =>
+    isSearchBoxShown() ? hideSearchBox() : showSearchBox()
+
+  const isSearchBoxShown = () =>
+    body.classList.contains('show-search-box')
+
+  const showSearchBox = () =>
+    body.classList.add('show-search-box')
+
+  const hideSearchBox = () =>
+    body.classList.remove('show-search-box')
+
+  document
+    .getElementById('search-button')
+    .addEventListener('click', event => {
+      event.stopPropagation()
+      menuVisibilityCheckbox.checked = false
+      searchTextBox.value || toggleSearchBox()
+    }, false)
+
+  document
+    .getElementById('search-option-container')
+    .addEventListener('click', event => event.stopPropagation(), false)
+
+  document.addEventListener('click', () => searchTextBox.value || hideSearchBox(), false)
+  searchTextBox.addEventListener('keydown', onkeydown, false)
+  searchTextBox.addEventListener('click', event => event.stopPropagation(), false)
+
+  function onkeydown ({keyCode}) {
+    if (keyCode === 27) {
+      if (searchTextBox.value) {
+        searchTextBox.value = ''
+      } else {
+        hideSearchBox()
+      }
+    }
+  }
+}).call(window, window)
