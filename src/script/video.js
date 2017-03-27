8666cd584c8d73030adc4887cc284459ca4fd42d
khai96_ <hvksmr1996@gmail.com>
2017-03-27 07:44:42 +0700

    Move close->exitFullscreen to lib.js

--- src/script/video.js
+++ src/script/video.js
@@ -1,13 +1,11 @@
 (function (window) {
   'use strict'
-  const {document, polyfill, mediaCommonAction} = window
-  const {fullscreen} = polyfill
+  const {document, mediaCommonAction} = window
   const subpage = document.documentElement.getAttribute('subpage-name')
   mediaCommonAction('video', 'video/mp4', '../media/video/' + subpage, {onOpenMedia})
 
-  function onOpenMedia ({player, close}) {
+  function onOpenMedia ({player}) {
     setTimeout(() => player.play(), 500)
     player.controls = player.autoplay = true
-    close.addEventListener('click', fullscreen.exit, false)
   }
 }).call(window, window)
