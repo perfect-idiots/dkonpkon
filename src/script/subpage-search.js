91bb965c0b33a7d02165bea51171454586fac982
khai96_ <hvksmr1996@gmail.com>
2017-03-31 07:33:42 +0700

    Move functions to lib.js from subpage-search.js

--- src/script/subpage-search.js
+++ src/script/subpage-search.js
@@ -1,33 +1,10 @@
 (function (window) {
   'use strict'
-  const {document} = window
+  const {document, search: {check}} = window
   const searchTextBox = document.getElementById('search-input')
   const list = document.querySelectorAll('#main-list .article-container')
   const filterFieldSelect = document.getElementById('filter-field-select')
   const caseSensitiveCheckbox = document.getElementById('case-sensitive-checkbox')
-
-  const diacritic = {
-    a: 'áàảãạăắằẳẵặâấầẩẫậ',
-    d: 'đ',
-    e: 'éèẻẽẹêếềểễệ',
-    i: 'íìỉĩị',
-    o: 'óòỏõọôốồổỗộơờởỡợ',
-    u: 'úùủũụưứừửữự',
-    y: 'ýỳỷỹỵ'
-  }
-  for (const nonDiaChar in diacritic) {
-    diacritic[nonDiaChar.toUpperCase()] = diacritic[nonDiaChar].toUpperCase()
-  }
-
-  const reverseDiacritic = {}
-  for (const nonDiaChar in diacritic) {
-    for (const diaChar of diacritic[nonDiaChar]) {
-      reverseDiacritic[diaChar] = nonDiaChar
-    }
-  }
-
-  const getNonDiaStr = diaString =>
-    Array.from(diaString).map(diaChar => reverseDiacritic[diaChar] || diaChar).join('')
 
   searchTextBox.showSearchResult = showSearchResult
 
@@ -54,14 +31,6 @@
     for (const articleContainer of list) {
       const content = getText(getContentElement(articleContainer).textContent)
       articleContainer.hidden = check(content, text)
-    }
-
-    function check (content, text) {
-      return notSubStr(content, text) && notSubStr(getNonDiaStr(content), text)
-    }
-
-    function notSubStr (container, substring) {
-      return container.indexOf(substring) === -1
     }
   }
 }).call(window, window)
